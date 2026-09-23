bdefa925b860f1d96468ec878f8f6cae20e6a847
cconner57 <github.gothic155@passmail.net>
2026-09-22 20:19:14 -0700

    refactor(footer): remove submit happy tail button from footer navigation

--- src/components/common/footer/Footer.vue
+++ src/components/common/footer/Footer.vue
@@ -16,7 +16,6 @@ const currentYear = computed(() => new Date().getFullYear())
           <RouterLink to="/volunteer">Volunteer</RouterLink>
           <RouterLink to="/donate">Donate</RouterLink>
           <RouterLink to="/happy-tails">Happy Tails</RouterLink>
-          <RouterLink to="/happy-tails?submit=true" class="footer-highlight-link">Submit Happy Tail 🐾</RouterLink>
           <RouterLink to="/wishlist">Wishlist</RouterLink>
           <RouterLink to="/news">News</RouterLink>
           <RouterLink to="/medical-records">Medical Records</RouterLink>
